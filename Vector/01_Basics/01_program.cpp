1065c4a0c067dfa84761cc52fb3013168a2cb4c2
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2025-07-14 16:29:11 +0530

    add basic program

--- Vector/01_Basics/01_program.cpp
+++ Vector/01_Basics/01_program.cpp
@@ -3,7 +3,24 @@
 using namespace std;
 
 int main() {
-  vector<int> vec; // the size of vectore is 0;
-  cout<<vec.size();
+  vector<int> vec;  // the size of vector is 0
+
+  vec.push_back(1);
+  vec.push_back(2);
+  vec.push_back(3);
+  vec.push_back(4);
+  vec.emplace_back(5);
+  vec.pop_back();
+
+  for (int val : vec) {
+    cout << val << " ";
+  }
+
+  cout << "\nSize : " << vec.size();
+  cout << "\nCapacity : " << vec.capacity();
+  cout << "\nValue at index 2 is : " << vec.at(2);
+  cout << "\nFont Value is : " << vec.front()
+       << "\nBack Value is : " << vec.back();
+
   return 0;
 }
